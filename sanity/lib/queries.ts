3e32ee4272d165feb84af8f9607c03de7b204dbb
grisha <rudenko@tap2visit.com>
2024-12-21 22:20:07 +0300

    feat(): real time search (search by whole word)

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -2,7 +2,7 @@ import {defineQuery} from "groq";
 
 export const STARTUPS_QUERY =
     defineQuery(`
-    *[_type == "startup" && defined(slug.current)]{
+    *[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc){
   _id,
     title,
     slug,
