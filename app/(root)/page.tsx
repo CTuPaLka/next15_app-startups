98749bcd2b2d2100aeb28508daf315ca20eb38b7
grisha <rudenko@tap2visit.com>
2024-12-21 21:46:41 +0300

    feat(): cache & live sanity api

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,14 @@
 import {SearchForm} from "@/components/SearchForm";
 import {SearchFormReset} from "@/components/SearchFormReset";
 import {StartupCard, StartupCardType} from "@/components/StartupCard";
-import {client} from "@/sanity/lib/client";
 import {STARTUPS_QUERY} from "@/sanity/lib/queries";
+import {sanityFetch, SanityLive} from "@/sanity/lib/live";
 
 export default async function Home({ searchParams }: {
     searchParams: Promise<{ query?: string }>
 }) {
 const query = (await searchParams).query;
-const posts = await client.fetch(STARTUPS_QUERY)
+const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
 
   return (
       <>
@@ -32,6 +32,7 @@ const posts = await client.fetch(STARTUPS_QUERY)
                   )}
               </ul>
           </section>
+          <SanityLive />
       </>
   )
 }
--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,5 +6,5 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation (means that if true every 60 sec content will be revalidated. data is cashed.)
 })
